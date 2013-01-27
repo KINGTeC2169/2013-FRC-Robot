132491eb176f3bd69484f6c010612572f1eebdd0
Mason Tran <lmnotran@gmail.com>
2013-01-27 17:44:05 -0600

    Moved readAccelerometer.ido into its own project folder. Functions moved into respective ino files to help with organization Added +/- 2g to 16g range bytes to address table (currently using +/- 4g)

---
+++ Arduino/readAccelerometer/readWriteFunctions.ino
@@ -0,0 +1,36 @@
+void writeTo(byte address, byte val) {
+  Wire.beginTransmission(DEVICE); // start transmission to device
+  Wire.write(address); // send register address
+  Wire.write(val); // send value to write
+  Wire.endTransmission(); // end transmission
+}
+
+// Reads num bytes starting from address register on device in to _buff array
+void readFrom(byte address, int num, byte _buff[]) {
+  Wire.beginTransmission(DEVICE); // start transmission to device
+  Wire.write(address); // sends address to read from
+  Wire.endTransmission(); // end transmission
+
+  Wire.beginTransmission(DEVICE); // start transmission to device
+  Wire.requestFrom(DEVICE, num); // request 6 bytes from device
+
+  int i = 0;
+  while(Wire.available()) // device may send less than requested (abnormal)
+  {
+    _buff[i] = Wire.read(); // receive a byte
+    i++;
+  }
+  Wire.endTransmission(); // end transmission
+}
+
+void readAccel() {
+  uint8_t howManyBytesToRead = 6;
+  readFrom( DATAX0, howManyBytesToRead, _buff); //read the acceleration data from the ADXL345
+
+  // each axis reading comes in 10 bit resolution, ie 2 bytes. Least Significat Byte first!!
+  // thus we are converting both bytes in to one int
+  int x = (((int)_buff[1]) << 8) | _buff[0];
+  int y = (((int)_buff[3]) << 8) | _buff[2];
+  int z = (((int)_buff[5]) << 8) | _buff[4];
+}
+
